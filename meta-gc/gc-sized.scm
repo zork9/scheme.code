30333022d73ed07f81ca7c60de4fdccf37a2edf0
Johan Ceupens <johan@sigtrans.org>
2013-12-18 15:55:26 +0100

    gc compiles

--- meta-gc/gc-sized.scm
+++ meta-gc/gc-sized.scm
@@ -19,79 +19,80 @@
 
 
 (define (make-sized-gc)
-	(let ((*heap '())
-		(*null '())
-		(*max-datasize 1024)	
-		(*datasize 0)	
-		(*currentn 0)	
-		(*max-chunksize 8)	
-		(*dynamic-data '())	
-		)
+  (let ((*heap '())
+	(*null '())
+	(*max-datasize 1024)	
+	(*datasize 0)	
+	(*currentn 0)	
+	(*max-chunksize 8)	
+	(*dynamic-data '())	
+	)
+    
+    (define (malloc)
+      (add-to-heap! 1)
+      )
 
-	(define (malloc)
-		(add-to-heap! 1)
-		)
-	(define (calloc nchunks)
-		(add-to-heap! nchunks)
-		)
+    (define (calloc nchunks)
+      (add-to-heap! nchunks)
+      )
 
-
-	;; private procedures
-
-	(define (get-heap) *heap)
-
-	;; data is a string
-	(define (get-data-size d) (string-length d))
-
-	(define (generate-error msg)
-		(cond ((eq? msg 'memory-exhausted)
-			(display "memory exhausted)(newline))
-		)	
-
-	(define (add-to-heap! n)
-		(do ((n2 0 (+ n2 1)))
-			((>= n2 n) *heap)
-			(set! *heap (append (get-heap) (list (make-chunk)))))
-
-	(define (make-chunk)
-		(cons 'chunk *null))  
-
-	;;NOTE append null string after <8
-	;; data is a string
-	(define (split-data data)
-		(let ((s ""))
-		(let ((retl '())
-		(do ((n2 0 (+ n2 1)))
-			((>= n2 (string-length data))
-			retl)
-		(do ((n 0 (+ n 1)))
-			((or (>= n 8)(>= (+ n2 n) (string-length data)))
-				 (append retl (list s))(set! n2 (+ n2 n)))
-			(string-append s (string-ref data n))
-			)))
-
-	;; FIX over *currentn
-	(define (set-data! chunk data)
-		(let ((lst (split-data data)))
-
-		(do ((n 0 (+ n 1)))
-			((>= n (length lst)) #t)
-			(set! (list-ref (get-heap) (+ n *currentn)) 
-			(list-ref lst n))) 
-		(set! *currentn (+ n *currentn)))
-
-	(define (set-chunk-data! chunk)
-		(set! *datasize (+ *datasize (get-data-size chunk))))
-		(if (> *datasize *max-datasize) 
-			(generate-error 'memory-exhausted)
-			;; The data is dynamically bound in this actor
-			(set-data! chunk *dynamic-data)
-			))
-
-	(define (dispatch msg)
-		(cond ((eq? msg 'malloc) malloc)	
-		      ((eq? msg 'calloc) calloc)
-	
-			(else (display "make-gc : message not understood : ")(display msg)(newline))
-		))
-	))		
+    ;; private procedures
+    
+    (define (get-heap) *heap)
+    
+    ;; data is a string
+    (define (get-data-size d) (string-length d))
+    
+    (define (generate-error msg)
+      (cond ((eq? msg 'memory-exhausted)
+	     (display "memory exhausted")(newline))
+	    ))
+    
+    (define (add-to-heap! n)
+      (do ((n2 0 (+ n2 1)))
+	  ((>= n2 n) *heap)
+	(set! *heap (append (get-heap) (list (make-chunk)))))
+      )
+    
+    (define (make-chunk)
+      (cons 'chunk *null))  
+    
+    ;;NOTE append null string after <8
+    ;; data is a string
+    (define (split-data data)
+      (let ((s ""))
+	(let ((retl '()))
+	  (do ((n2 0 (+ n2 1)))
+	      ((>= n2 (string-length data))
+	       retl)
+	    (do ((n 0 (+ n 1)))
+		((or (>= n 8)(>= (+ n2 n) (string-length data)))
+		 (append retl (list s))(set! n2 (+ n2 n)))
+	      (string-append s (string-ref data n))
+	      ))
+	  )))
+  
+    ;; FIX over *currentn
+    (define (set-data! chunk data)
+      (let ((lst (split-data data)))
+	(do ((n 0 (+ n 1)))
+	    ((>= n (length lst)) #t)
+	  (set! (list-ref (get-heap) (+ n *currentn)) 
+		(list-ref lst n))) 
+	(set! *currentn (+ n *currentn))
+	))
+    
+    (define (set-chunk-data! chunk)
+      (set! *datasize (+ *datasize (get-data-size chunk)))
+      (if (> *datasize *max-datasize) 
+	  (generate-error 'memory-exhausted)
+	  ;; The data is dynamically bound in this actor
+	  (set-data! chunk *dynamic-data)
+	  ))
+    
+    (define (dispatch msg)
+      (cond ((eq? msg 'malloc) malloc)	
+	    ((eq? msg 'calloc) calloc)
+	    (else (display "make-gc : message not understood : ")(display msg)(newline))
+	    ))
+    dispatch))		
