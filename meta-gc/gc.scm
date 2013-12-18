cd4ab2f40d1c378ad4ce471def741c6523f39ef9
Johan Ceupens <johan@sigtrans.org>
2013-12-18 11:20:30 +0100

    init metacircular gc

---
+++ meta-gc/gc.scm
@@ -0,0 +1,63 @@
+;; Copyright (C) Johan Ceuppens 2013
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define (make-gc)
+	(let ((*heap '())
+		(*null '())
+		(*max-datasize 1024)	
+		(*datasize 0)	
+		(*dynamic-data '())	
+		)
+
+	(define (malloc)
+		(add-to-heap! 1)
+		)
+	(define (calloc nchunks)
+		(add-to-heap! nchunks)
+		)
+
+
+	;; private procedures
+
+	(define (get-heap) *heap)
+
+	(define (get-data-size d) (length d))
+
+	(define (generate-error msg)
+		(cond ((eq? msg 'memory-exhausted)
+			(display "memory exhausted)(newline))
+		)	
+
+	(define (add-to-heap! n)
+		(set! *heap (append (get-heap) (list (make-chunk)))))
+
+	(define (make-chunk)
+		(cons 'chunk *null))  
+
+	(define (set-chunk-data! chunk)
+		(set! *datasize (+ *datasize (get-data-size chunk))))
+		(if (> *datasize *max-datasize) 
+			(generate-error 'memory-exhausted)
+			;; The data is dynamically bound in this actor
+			(set-data! chunk *dynamic-data)
+			))
+
+	(define (dispatch msg)
+		(cond ((eq? msg 'malloc) malloc)	
+		      ((eq? msg 'calloc) calloc)
+	
+			(else (display "make-gc : message not understood : ")(display msg)(newline))
+		))
+	))		
---
+++ meta-gc/meta-gc.scm
@@ -0,0 +1,42 @@
+;; Copyright (C) Johan Ceuppens 2013
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(load "gc.scm")
+
+(define (make-meta-gc)
+	(let ((*gc-program (make-gc))
+		)
+
+	;; interrupt system : FIXME sort of hooks
+	(define (signal number)
+		(cond ((= number 9)
+			(display "exiting meta-gc...")
+			(exit))
+		      ((= number 7)
+			(display "exiting meta-gc...")
+			(exit))
+	              (else (display "make-meta-gc : SIGNAL == ")(display number)(display " unknown signal to gc")
+				(newline)	
+			)))
+
+	(define (dispatch msg)
+		(cond ((eq? msg 'run) run)
+		      ((eq? msg 'signal) signal)
+		      ((eq? msg 'int) signal)
+		      ((eq? msg 'interrupt) signal)
+
+		      (else (display "make-meta-gc : message not understood : ")(display msg)(newline)
+		))
+	)) 
